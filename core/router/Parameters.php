3119889b87731b827d9fd18d8b282763ce8b55b4
Felipe Canever Fernandes <felipe.canever.fernandes@outlook.com>
2021-05-11 17:23:43 -0300

    Make Parameters convert action name to camel case

--- core/router/Parameters.php
+++ core/router/Parameters.php
@@ -10,7 +10,7 @@ class Parameters
     public function __construct(string $controller, string $action = 'index')
     {
         $this->controller = self::validateController($controller);
-        $this->action = self::validateParameter($action);
+        $this->action = self::validateAction($action);
     }
 
     public function getController(): string
@@ -30,7 +30,7 @@ class Parameters
 
     public function setAction(string $action): void
     {
-        $this->action = self::validateParameter($action);
+        $this->action = self::validateAction($action);
     }
 
     private static function validateParameter(string $parameter): string
@@ -44,10 +44,20 @@ class Parameters
         return self::toPascalCase(self::validateParameter($controller));
     }
 
+    private static function validateAction(string $action): string
+    {
+        return self::toCamelCase(self::validateParameter($action));
+    }
+
     private static function toPascalCase(string $string): string
     {
         $words = str_replace('-', ' ', $string);
         $capitalized = ucwords($words);
         return str_replace(' ', '', $capitalized);
+    }
+
+    private static function toCamelCase(string $string): string
+    {
+        return lcfirst(self::toPascalCase($string));
     }
 }
