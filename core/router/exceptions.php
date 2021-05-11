3ca04513fcbc860be81cd79aef16e44ff5401ae6
Felipe Canever Fernandes <felipe.canever.fernandes@outlook.com>
2021-05-11 17:55:22 -0300

    Make ActionNotFoundException take action as argument

--- core/router/exceptions.php
+++ core/router/exceptions.php
@@ -7,9 +7,9 @@ use Throwable;
 
 class ActionNotFoundException extends Exception
 {
-    public function __construct(string $message = "", int $code = 0, Throwable $previous = null)
+    public function __construct(string $action, int $code = 0, Throwable $previous = null)
     {
-        parent::__construct($message, $code, $previous);
+        parent::__construct("Action '$action' not found.", $code, $previous);
     }
 }
 
