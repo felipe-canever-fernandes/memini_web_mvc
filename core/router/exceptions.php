12eabe25b620f2d053933bda0d0ac40729353d1c
Felipe Canever Fernandes <felipe.canever.fernandes@outlook.com>
2021-05-11 17:44:19 -0300

    Create ControllerNotFoundException

--- core/router/exceptions.php
+++ core/router/exceptions.php
@@ -5,6 +5,14 @@ namespace Core\Router;
 use Exception;
 use Throwable;
 
+class ControllerNotFoundException extends Exception
+{
+    public function __construct(string $message = "", int $code = 0, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+}
+
 class RouteNotFoundException extends Exception
 {
     public function __construct(string $message = "", int $code = 0, Throwable $previous = null)
