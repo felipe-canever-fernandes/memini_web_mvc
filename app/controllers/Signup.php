67fba4448ab02fd79616e47e5c010530fa55125b
Felipe Canever Fernandes <felipe.canever.fernandes@outlook.com>
2021-05-16 15:52:03 -0300

    Implement signup

--- app/controllers/Signup.php
+++ app/controllers/Signup.php
@@ -2,8 +2,7 @@
 
 namespace App\Controllers;
 
-use App\Models\Post;
-
+use App\Models\User;
 use Core\Controller;
 use Core\View\View;
 
@@ -12,5 +11,16 @@ class Signup extends Controller
     public function indexAction(): void
     {
         View::render('signup/index.twig');
+    }
+
+    public function insertAction(): void
+    {
+        $user = new User(
+            $_POST['name'],
+            $_POST['email'],
+            password_hash($_POST['password'], PASSWORD_DEFAULT)
+        );
+
+        User::insert($user);
     }
 }
