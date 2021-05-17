ea6693298839fefcadceb513fbb8e3ae16467098
Felipe Canever Fernandes <felipe.canever.fernandes@outlook.com>
2021-05-17 14:11:43 -0300

    Make controller Users require the user to be administrator

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -2,11 +2,18 @@
 
 namespace App\Controllers;
 
+use App\Authorization;
 use Core\Controller;
 use Core\View\View;
 
 class Users extends Controller
 {
+    public function doBefore(): bool
+    {
+        Authorization::request();
+        return true;
+    }
+
     public function indexAction(): void
     {
         View::render('users/index.twig');
