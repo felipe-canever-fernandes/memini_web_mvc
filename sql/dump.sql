9e109fe06f958a483896a99a34f8bf74daf9c311
Felipe Canever Fernandes <felipe.canever.fernandes@outlook.com>
2021-05-17 13:19:24 -0300

    Write SQL for creating user table

--- sql/dump.sql
+++ sql/dump.sql
@@ -4,4 +4,16 @@ DROP DATABASE IF EXISTS `avii_desenvweb`;
 
 CREATE DATABASE `avii_desenvweb`;
 
+-- User
+CREATE TABLE `avii_desenvweb`.`user`(
+    `user_id`	        INT 			NOT NULL	AUTO_INCREMENT,
+    `name`	            VARCHAR(255)	NOT NULL,
+    `email`	            VARCHAR(127)	NOT NULL,
+    `hashed_password`	VARCHAR(255)	NOT NULL,
+    `is_administrator`  BOOLEAN         NOT NULL    DEFAULT FALSE,
+
+    PRIMARY KEY (`user_id`),
+    UNIQUE KEY  (`email`)
+);
+
 COMMIT;
