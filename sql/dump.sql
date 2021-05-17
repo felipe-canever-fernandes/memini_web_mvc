d96f295e236c0e6b477c7004590245797109319b
Felipe Canever Fernandes <felipe.canever.fernandes@outlook.com>
2021-05-17 13:25:13 -0300

    Write SQL for inserting administrator

--- sql/dump.sql
+++ sql/dump.sql
@@ -16,4 +16,8 @@ CREATE TABLE `avii_desenvweb`.`user`(
     UNIQUE KEY  (`email`)
 );
 
+-- Users
+INSERT INTO `avii_desenvweb`.`user` (`name`,	    `email`,                                `hashed_password`,                                              `is_administrator`)
+VALUES								("Felipe",		"felipe.canever.fernandes@outlook.com", "$2y$10$33buAp7lQlCf2AetVfKjeuJ6AR9zf7PtoNqVZKRKuqm.Xi4wPL52K", TRUE);
+
 COMMIT;
