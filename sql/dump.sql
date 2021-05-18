0412aaa845aab4bd198742520ede05697f19ead9
Felipe Canever Fernandes <felipe.canever.fernandes@outlook.com>
2021-05-18 13:22:44 -0300

    Write SQL for inserting additional users

--- sql/dump.sql
+++ sql/dump.sql
@@ -17,9 +17,13 @@ CREATE TABLE `avii_desenvweb`.`user`(
 );
 
 -- Users
-INSERT INTO `avii_desenvweb`.`user` (`name`,	    `email`,                                `hashed_password`,                                              `is_administrator`)
---                                                                                          060193
-VALUES								("Felipe",		"felipe.canever.fernandes@outlook.com", "$2y$10$33buAp7lQlCf2AetVfKjeuJ6AR9zf7PtoNqVZKRKuqm.Xi4wPL52K", TRUE);
+INSERT INTO `avii_desenvweb`.`user` (`name`,	    `email`,                                    `hashed_password`,                                              `is_administrator`)
+--                                                                                               060193
+VALUES								("Felipe",		"felipe.canever.fernandes@outlook.com",     "$2y$10$33buAp7lQlCf2AetVfKjeuJ6AR9zf7PtoNqVZKRKuqm.Xi4wPL52K", TRUE),
+--                                                                                               081293
+                                    ("Lucas",		"lucas.canever.fernandes@outlook.com",      "$2y$10$tZsYlcu6mGp/SblPIO5KZecgR7axryX/Le8IQv1H98PWIxKA0rCeO", FALSE),
+--                                                                                               041200
+                                    ("Matheus",		"matheus.canever.fernandes@outlook.com",    "$2y$10$9JUyCEwUUXksBmLXEDdp9.eSI24scg3aR8nKCspWP0M5KMSs4.BYy", FALSE);
 
 -- Deck
 CREATE TABLE `avii_desenvweb`.`deck`(
