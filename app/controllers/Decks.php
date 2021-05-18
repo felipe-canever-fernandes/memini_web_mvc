1900f3e1337fd219a5834c0c4c4ca993fac69ed4
Felipe Canever Fernandes <felipe.canever.fernandes@outlook.com>
2021-05-18 14:38:55 -0300

    Create decks page

---
+++ app/controllers/Decks.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Authentication;
+use App\Models\Deck;
+use Core\Controller;
+use Core\View\View;
+
+class Decks extends Controller
+{
+    public function indexAction(): void
+    {
+        $decks = Deck::findAllByUser(Authentication::getSignedInUser()->getId());
+        View::render('decks/index.twig', ['decks' => $decks]);
+    }
+}
