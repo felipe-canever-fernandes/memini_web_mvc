f44dec5871cd7857237c365e19e7a289a1bc7837
Felipe Canever Fernandes <felipe.canever.fernandes@outlook.com>
2021-05-18 14:44:21 -0300

    Redirect user to decks page when going to home and signed in

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -2,14 +2,19 @@
 
 namespace App\Controllers;
 
+use App\Authentication;
 use App\Models\User\User;
 use Core\Controller;
+use Core\Router\Router;
 use Core\View\View;
 
 class Home extends Controller
 {
     public function indexAction(): void
     {
+        if (Authentication::isUserSignedIn())
+            Router::redirect('/decks');
+
         View::render('home/index.twig');
     }
 }
