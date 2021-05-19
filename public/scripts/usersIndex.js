f179c7ee70a81701b9017c02e7bb65a3b49faf29
Felipe Canever Fernandes <felipe.canever.fernandes@outlook.com>
2021-05-19 11:11:51 -0300

    Remove leftover function from usersIndex.js

--- public/scripts/usersIndex.js
+++ public/scripts/usersIndex.js
@@ -1,13 +1,6 @@
 const userNameSpan = $('#userNameSpan');
 const userIdInput = $('#userIdInput');
 
-function toggleVisibility() {
-    eye.toggleClass('bi-eye-fill bi-eye-slash');
-
-    const type = password.prop('type') === 'password' ? 'text' : 'password';
-    password.prop('type', type);
-}
-
 function displayModal(userId, userName, userEmail) {
     userNameSpan.text(`${userEmail} (${userName})`);
     userIdInput.val(userId);
